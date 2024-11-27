0c684c1d0afea22b5cc06824483b4cff1e6a1000
Terry Brady <terrywbrady@gmail.com>
2024-11-27 09:10:25 -0800

    refine test config

--- config.sh
+++ config.sh
@@ -1,19 +1,19 @@
 # Need to investigate why login time can be so high
 MS_LOGIN=60000
-MS_RESP=2000
-DURATION=3m
+MS_RESP=4000
+DURATION=2m
 
 if [[ "$LOCUST_TEST_ENV" == "production" ]]
 then
   MERRITTURL=https://merritt.cdlib.org
   SSMPATH=/uc3/mrt/dev/integ-tests/for-prod
-  USERCOUNT=10
+  USERCOUNT=6
   ARKLIST=ark:/99999/fk4t16pn3j,ark:/99999/fk4xp8q22b
 elif [[ "$LOCUST_TEST_ENV" == "preview" ]]
 then
   MERRITTURL=https://uc3-mrt-preview-prd.cdlib.org
   SSMPATH=/uc3/mrt/dev/integ-tests/for-prod
-  USERCOUNT=10
+  USERCOUNT=6
   ARKLIST=ark:/99999/fk4t16pn3j,ark:/99999/fk4xp8q22b
 else
   MERRITTURL=https://merritt-stage.cdlib.org
